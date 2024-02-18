24ee9dd3e9cdfeac06080beba86bdd7f7636e103
Pavlo <pavlo.maksymov@spryker.com>
2024-02-18 12:25:05 +0100

    Changed k8s provider info

--- modules/kube_blog/main.tf
+++ modules/kube_blog/main.tf
@@ -1,8 +1,19 @@
-data "aws_eks_cluster" "blog" {
-  name = "blog"
+data "aws_eks_cluster" "example" {
+  name = "example"
 }
-data "aws_eks_cluster_auth" "blog" {
-  name = "blog"
+
+data "aws_eks_cluster_auth" "example" {
+  name = "example"
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.example.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.example.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    command     = "aws"
+  }
 }
 
 resource "kubernetes_namespace" "test" {
